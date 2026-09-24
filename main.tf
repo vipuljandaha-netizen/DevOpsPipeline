2fef21b8b7c55e34443c7f7d8e6400e14edd7661
vipul kumar <Vipulkumar.Choudhary@b18g61.online>
2026-09-24 11:08:32 +0530

    Add Azure Terraform configuration

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "rgs" {
+  name     = "poc-rg"
+  location = "Central India"
+}
+
+#resource "azurerm_resource_group" "rg" {
+# name     = "nprd-rg"
+#location = "Central India"
+#}
